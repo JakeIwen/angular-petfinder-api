5b6cc92712140ec881090e45813e4eadc208bfa6
jakeiwen <iwenx006@umn.edu>
2016-11-30 13:41:12 -0600

    birds and cats. split controllers

---
+++ server/public/scripts/controllers/tinylions.js
@@ -0,0 +1,27 @@
+app.controller('TinyLionController', ['$http', function($http) {
+  console.log('tinylions controller up and running');
+  var key = 'bfe9d3977a90f8c813e56978bcfbec99';
+  var self = this;
+
+  self.animal = {};
+
+  self.getRandomPet = function(){
+    var query = 'https://api.petfinder.com/'; // baseURL for API
+    query += 'pet.getRandom'; // selecting the method we would like to return
+    query += '?key=' + key; // Giving petfinder our key
+    query += '&format=json'; // Telling petfinder we want our response to be JSON
+    query += '&animal=cat';
+    query += '&output=basic'; // Telling petfinder what data we want (more than just id)
+    var request = encodeURI(query) + '&callback=JSON_CALLBACK'; // removing spaces and special characters from response as well as making jsonp work with the callback
+
+    console.log('Request:', request);
+
+    $http.jsonp(request).then(function(response){
+      console.log(response);
+      self.animal = response.data.petfinder.pet;
+    });
+
+  }
+
+  self.getRandomPet();
+}]);
